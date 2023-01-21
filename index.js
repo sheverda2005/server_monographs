f294a296c2b95ccb30eacb9cff02c28a6318529d
Sheverda <nsewerda04@gmail.com>
2023-01-21 19:26:35 +0200

    cookie

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
     credentials: true,
-    origin: "https://monographs.vercel.app"
+    origin: "http://localhost:3000"
 }))
 app.use("/api", router)
 
