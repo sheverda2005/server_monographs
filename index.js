42a46647916b269748eb0363121ebbe8b4431c96
Sheverda <nsewerda04@gmail.com>
2023-02-18 21:18:08 +0200

    vercel-cors

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express")
 const mongoose = require("mongoose");
 const router = require("./routers/router");
 const cookieParser = require("cookie-parser")
-const cors = require("cors")
+const cors = require("vercel-cors")
 
 const app = express()
 
@@ -12,10 +12,7 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors({
-    credentials: true,
-    origin: true
-}))
+app.use(cors())
 app.use("/api", router)
 
 async function start() {
